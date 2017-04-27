93a6dccc385c24cbc9d910d3268aaa8d962e6db9
nitramnoe <eon.martin@icloud.com>
2017-04-27 10:12:59 +0200

    ajout de signup

--- client/src/Components/buttonLogin.jsx
+++ client/src/Components/buttonLogin.jsx
@@ -8,17 +8,31 @@ const animConnectForm = keyframes`
     opacity: 1;
   }
 `;
+const animEmail = keyframes`
+  0%{
+    height:0%;
+    width: 0%;
+    font-size: 0;
+    opacity: 0;
+  }50%{
+    width: 200px;
+    height: 40px;
+    font-size: 25px;
+    opacity: 0;
+  }
+`;
 
 const ButtonLogin = styled.button`
   width: 150px;
   height: 40px;
   background-color: transparent;
-  border: 2px solid ${props => props.inputColor};;
-  border-radius: 10px;
-  color: ${props => props.inputColor};;
+  border: 2px solid ${props => props.Color};;
+  border-radius: 5px;
+  color: ${props => props.Color};;
   font-size: 15px;
   display: flex;
   justify-content: center;
+  outline: none;
   align-items: center;
   cursor: pointer;
   text-decoration: none;
@@ -29,25 +43,29 @@ const ConnectWrapper = styled.div`
   top: 0;
   left:0;
   position: fixed;
-  background-color: rgba(89, 89, 89, 0.33);
+  background-color: rgba(109, 109, 89, 0.63);
   display: ${props => props.isClicked? 'flex' : 'none'};
   justify-content: center;
   align-items: center;
   flex-direction: column;
   animation: ${animConnectForm} 2s ease 1;
+  z-index: 1000;
 `;
+
 const ConnectInput = styled.input`
   width: 200px;
   height: 40px;
   border-radius: 5px;
   background-color: transparent;
-  border: 2px solid ${props => props.inputColor};
-  color: ${props => props.inputColor};
+  border: 2px solid ${props => props.Color};
+  color: ${props => props.Color};
+  outline: none;
   display: flex;
   justify-content: center;
   align-items: center;
   padding-left: 5px;
   font-size: 25px;
+  ${''/* animation: ${animEmail} 2s ease; */}
 `;
 const CloseIcon = styled.a`
   text-decoration: none;
@@ -55,6 +73,8 @@ const CloseIcon = styled.a`
   font-size: 70px;
   position: absolute;
   cursor: pointer;
+  font-weight: light;
+  font-family: helvetica neue;
   top: -10px;
   right: 10px;
 `;
@@ -70,29 +90,56 @@ const ConnectForm = styled.form`
   justify-content: space-around;
   flex-direction: column;
   align-items: center;
-`
+`;
+const SwitchLogSign = styled.a`
+  color: ${props => props.Color};
+  font-size: 20px;
+  font-weight: light;
+  cursor: pointer;
+`;
 
 class Login extends Component {
   constructor(props){
     super(props);
     this.state = {
       isLoginClicked : false,
-      inputColor: 'rgb(86, 215, 142)'
+      Color: 'rgb(86, 215, 142)',
+      wantLogIn: true
     }
   }
+
+  switchLogSign = () => {
+    if(this.state.wantLogIn === true){
+      return this.setState({wantLogIn: false});
+    }else {
+      return this.setState({wantLogIn: true});
+    }
+  }
+
   render(){
     return(
       <div>
-        <ButtonLogin href="#" inputColor={this.state.inputColor} onClick={() => this.setState({isLoginClicked: true})}>
+        <ButtonLogin href="#" Color={this.state.Color} onClick={() => this.setState({isLoginClicked: true})}>
           Log In / Sign In
         </ButtonLogin>
         <ConnectWrapper isClicked={this.state.isLoginClicked}>
           <CloseIcon onClick={() => this.setState({isLoginClicked: false})} >x</CloseIcon>
-          <ConnectTitle inputColor={this.state.inputColor}>Log In</ConnectTitle>
-          <ConnectForm action='/login' method='post' >
-            <ConnectInput name='username' placeholder='UserName' inputColor='#fff '/>
-            <ConnectInput name='password' placeholder='PassWord' type='password' inputColor='#fff'/>
-            <ButtonLogin inputColor='#fff' type="submit">Connect</ButtonLogin>
+          <ConnectTitle Color={this.state.Color}>
+            {this.state.wantLogIn? 'Log In' : 'Sign In'}
+          </ConnectTitle>
+          <ConnectForm action={this.state.wantLogIn? '/login' : '/signin'} method='post' >
+
+            <ConnectInput name='username' placeholder='Username' Color='#fff'/>
+
+            {this.state.wantLogIn? '' : <ConnectInput name='email' placeholder='Email' Color='#fff'/>}
+
+            <ConnectInput name='password' placeholder='Password' type='password' Color='#fff'/>
+
+            <ButtonLogin Color='#fff' type="submit">Connect</ButtonLogin>
+
+            <SwitchLogSign onClick={this.switchLogSign} Color='#fff'>
+              {this.state.wantLogIn? 'Want to Sign In?' : 'Already User?'}
+            </SwitchLogSign>
           </ConnectForm>
         </ConnectWrapper>
       </div>
